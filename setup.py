db778d9d30315d60840a494e2f8a68793beadd4a
floren007 <florenrey@hotmail.es>
2024-07-15 17:28:15 +0200

    projectoml y setup

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="BicimadFlorentino",
     version="0.0.2",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=[
+          'pandas==2.2.2',
+          'matplotlib==3.9.1',
+          'requests == 2.1',
+      ]
 
 )
